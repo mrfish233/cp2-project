1c553ee430afc33f8105799206dc408947923913
arthur0824hao <arthur0961584197@gmail.com>
2024-05-18 15:24:53 +0800

    feat: update SDL.h and prepare to expand function of SDL.c

--- src/SDL.h
+++ src/SDL.h
@@ -4,15 +4,21 @@
 #include <SDL2/SDL_ttf.h>
 #include <stdio.h>
 #include <string.h>
+// SDL.h
+
+#ifndef SDL_H
+#define SDL_H
 
 typedef struct {
     SDL_Rect rect;  // 渲染區域的位置和大小
-    SDL_Texture* textures[3];  // 背景, 文字, 圖片
-    int currentTextureIndex;
-    char text[256];  // 儲存顯示的文字
-    SDL_Color textColor;
-    SDL_Rect textRect;  // 文字的位置和大小
-    SDL_Rect imageRect;  // 圖片的位置和大小
+    SDL_Texture* background;  // 背景紋理
+    SDL_Texture** images;  // 圖片紋理陣列
+    int image_count;  // 圖片數量
+    char** texts;  // 文字陣列
+    SDL_Texture** textTextures;  // 文字紋理陣列
+    SDL_Color* textColors;  // 文字顏色陣列
+    SDL_Rect* textRects;  // 文字位置和大小陣列
+    int text_count;  // 文字數量
 } RenderArea;
 
 typedef struct {
@@ -26,13 +32,14 @@ typedef struct {
     int switchCounter;  // 用於第三區域圖片切換的計數器
 } AppContext;
 
-
 void cleanUp(AppContext* ctx);
 void initSDL(AppContext* ctx);
 void loadTextures(AppContext* ctx);
-void renderText(AppContext* ctx, RenderArea* area);
-void renderBackground(AppContext* ctx, RenderArea* area);
+
 void renderImage(AppContext* ctx, RenderArea* area);
+void renderText(AppContext* ctx, RenderArea* area);
+void setRenderAreaContent(AppContext* ctx, int areaIndex, const char* background, char** images, int image_count, char** texts, SDL_Color* textColors, SDL_Rect* textRects, int text_count);
 void createRenderArea(AppContext* ctx, int x, int y, int w, int h);
 void renderAreas(AppContext* ctx);
-void setRenderAreaContent(AppContext* ctx, int areaIndex, const char* background, const char* image, const char* text, SDL_Color textColor, SDL_Rect textRect, SDL_Rect imageRect);
+
+#endif // SDL_H
